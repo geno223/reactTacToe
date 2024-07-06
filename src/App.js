651d292081c460fbfcc1d50226cd6175fa0b1da3
geno223 <eugenio.jimenez22397@gmail.com>
2024-07-06 14:25:36 -0400

    styling & added link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Player from './components/Players';
 import Board from './components/Board';
+import "./styles.css"
 ///header
 ///players
 ///9 squares
@@ -14,6 +15,7 @@ function App() {
      <Player whichPlayer= 'X'/>
      <Player whichPlayer='O'/>
      <Board/>
+     <a href='https://github.com/geno223'>Click me</a>
     </div>
   );
 }
