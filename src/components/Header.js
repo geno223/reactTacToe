e77bab59800af8eeed4338499b41ea00bb043526
geno223 <eugenio.jimenez22397@gmail.com>
2024-07-06 13:39:35 -0400

    passingProps

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+
+
+const Header =() => {
+    return <h1>React Tac Toe</h1>
+}
+
+
+export default Header;
---
+++ src/components/Players.js
@@ -0,0 +1,12 @@
+
+
+const Player =(props) => {
+    return(
+        <div>
+            <h2>Player {props.whichPlayer}</h2>
+            <h3>Wins:</h3>
+        </div>
+    )
+}
+
+export default Player
